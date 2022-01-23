6f119b0e8b666bfb3f9f4e3854739524cb64d660
Ajay Kathwate <71059395+ajaykathwate@users.noreply.github.com>
2022-01-23 09:37:55 +0530

    Time: 22 ms (96.83%), Space: 14.2 MB (53.17%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.py
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -1,12 +1,12 @@
 class Solution:
     @lru_cache(None)
     def sequentialDigits(self, low: int, high: int) -> List[int]:
-        res = []
+        res = set()
         
         for num in range(1,9):
             while num <= high and num%10 != 0:
                 if num >= low:
-                    res.append(num)
+                    res.add(num)
                 num = (num*10) + (num%10) + 1
             
         return sorted(res)
