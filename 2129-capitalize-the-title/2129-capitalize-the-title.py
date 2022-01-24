a4c476bda7630a84227067e492ebf8760b36e1d0
Ajay Kathwate <71059395+ajaykathwate@users.noreply.github.com>
2022-01-24 09:33:14 +0530

    Time: 44 ms (53.41%), Space: 14.3 MB (13.21%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.py
+++ 2129-capitalize-the-title/2129-capitalize-the-title.py
@@ -1,6 +1,3 @@
 class Solution:
     @lru_cache(None)
-    def capitalizeTitle(self, title: str) -> str:
-        lst = [(i[0].upper() + i[1:].lower())  if len(i) >= 3 else i.lower() for i in title.split(" ")]
-        
-        return " ".join(lst)
+    def capitalizeTitle(self, title: str) -> str:return " ".join([(i[0].upper() + i[1:].lower())  if len(i) >= 3 else i.lower() for i in title.split(" ")])
