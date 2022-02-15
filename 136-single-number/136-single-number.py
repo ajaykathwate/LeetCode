4020d40835cef1f8bc7d1a7c971d7df490f6b302
Ajay Kathwate <71059395+ajaykathwate@users.noreply.github.com>
2022-02-15 08:25:52 +0530

    Time: 153 ms (65.32%), Space: 16.7 MB (68.96%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,10 +1,13 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        c=Counter(nums)
-        for i in nums:
-            if c[i]==1:
-                return i
-            
+        return reduce(lambda x, y: x ^ y, nums)
+        
+        
+#        c=Counter(nums)
+#        for i in nums:
+#            if c[i]==1:
+#                return i
+        
         
 #        res = 0
 #        for n in nums:
