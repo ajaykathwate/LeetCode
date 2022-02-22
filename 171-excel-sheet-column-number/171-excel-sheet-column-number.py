4183eddc38c058747c1128ef84a2d56a2bcf6352
Ajay Kathwate <71059395+ajaykathwate@users.noreply.github.com>
2022-02-22 09:30:49 +0530

    Time: 44 ms (54.60%), Space: 13.8 MB (83.72%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.py
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.py
@@ -1,3 +1,11 @@
 class Solution:
     def titleToNumber(self, s: str) -> int:
-        return sum( (ord(char) - ord('A') + 1) *  26 ** i for i, char in enumerate( reversed(s) ) )
+        number = 0
+        for ch  in s:
+            dec = ord(ch) - ord('A') + 1
+            number = dec + number*26
+        return number
+        
+        
+        
+#        return sum( (ord(char) - ord('A') + 1) *  26 ** i for i, char in enumerate( reversed(s) ) )
