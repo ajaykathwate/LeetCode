e831d83146e43116e7e43fa69d16a6c66e2f22b3
Ajay Kathwate <71059395+ajaykathwate@users.noreply.github.com>
2022-03-09 10:45:15 +0530

    Time: 63 ms (41.57%), Space: 13.9 MB (85.61%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
@@ -5,19 +5,38 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        temp = []
-        cur = head
-        while cur:
-            temp.append(cur.val)
-            cur = cur.next
-            
-        c = Counter(temp)
-        temp = [k for k,v in c.items() if v == 1]
+        dummy = ListNode(0, next = head)
+        slow = dummy
         
-        dummy = cur = ListNode()
-        
-        for i in temp:
-            cur.next = ListNode(i)
-            cur = cur.next
-            
+        while head:
+            if head.next and head.val == head.next.val:
+                while head.next and head.val == head.next.val:
+                    head = head.next
+                slow.next = head.next
+            else:
+                slow = slow.next
+            head = head.next
         return dummy.next
+        
+        
+        
+        
+        
+# Naive Approach
+
+# temp = []
+#        cur = head
+#        while cur:
+#             temp.append(cur.val)
+#             cur = cur.next
+
+#         c = Counter(temp)
+#         temp = [k for k, v in c.items() if v == 1]
+
+#         dummy = cur = ListNode()
+
+#         for i in temp:
+#             cur.next = ListNode(i)
+#             cur = cur.next
+
+#         return dummy.next
