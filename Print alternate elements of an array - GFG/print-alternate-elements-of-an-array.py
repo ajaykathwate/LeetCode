6bcbbf245337d047d96c73f199f996f2623a9adf
Ajay Kathwate <71059395+ajaykathwate@users.noreply.github.com>
2022-03-20 12:45:26 +0530

    Added solution - LeetHub

---
+++ Print alternate elements of an array - GFG/print-alternate-elements-of-an-array.py
@@ -0,0 +1,23 @@
+#User function Template for python3
+
+# arr is the array
+# n is the number of elements in array
+def printAl(arr,n):
+    # your code here
+    for i in range(n):
+        if i%2 == 0:
+            print(arr[i], end=" ")
+    return 
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=="__main__":
+    t=int(input())
+    for i in range(t):
+        n=int(input())
+        arr=list(map(int,input().split()))
+        printAl(arr,n)
+        print()
+# } Driver Code Ends
