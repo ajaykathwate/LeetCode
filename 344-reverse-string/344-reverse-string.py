fe3d1f2cace4e32d8ef72a6aedcc03de55b79b75
Ajay Kathwate <71059395+ajaykathwate@users.noreply.github.com>
2022-04-01 11:28:47 +0530

    Time: 216 ms (78.31%), Space: 42 MB (8.13%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -3,4 +3,10 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        return s.reverse()
+        def helper(s,i):
+            if i >= len(s)//2:
+                return
+            s[i], s[len(s)-i-1] = s[len(s)-i-1], s[i]
+            helper(s, i+1)
+        helper(s,0)
+            
